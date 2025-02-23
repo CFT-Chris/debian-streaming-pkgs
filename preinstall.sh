be15342baf91e6df0af7bb4bfa1c0d32a32f9259
Chris <CFT-Chris@users.noreply.github.com>
2025-02-23 14:54:05 -0500

    Update preinstall.sh
    GStreamer missing packages

--- preinstall.sh
+++ preinstall.sh
@@ -3,6 +3,6 @@
 if [ "$(id -u)" = "0" ]; then
   apt-get update
   apt-get install -y python3 python3-dev python3-pip build-essential
-  apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev
+  apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
   apt-get install -y ffmpeg
 fi
